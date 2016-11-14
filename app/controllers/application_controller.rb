32c9a02e61efc02ac97891e76e237969b077fbbe
Max Wofford <max@maxwofford.com>
2016-11-13 22:12:06 -0500

    Add magic token to auth logic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,31 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
   protect_from_forgery with: :exception
+
+  protected
+
+  def authenticate_user!
+    return if current_user
+    maybe_login_from_token unless params[:token].blank?
+    if user_signed_in?
+      redirect_to request.path, params.except(:token, :action, :controller)
+    elsif params[:controller] == 'static_pages' and params[:action] == 'home'
+      return
+    else
+      redirect_to login_path, notice: "Invalid magic link"
+    end
+  end
+
+  def maybe_login_from_token
+    token = params[:token]
+    Rails.logger.info "maybe_login_from_token: '#{ token }'"
+
+    if (user = User.find_by_token(token))
+      Rails.logger.info "One time login token used for user #{ user.id }"
+      sign_in user
+      user.new_token!
+    else
+      Rails.logger.info "No user found from token: '#{ token }'"
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'static_pages#home'
+  root 'static_pages#home', as: :login
   get '/about', to: 'static_pages#about'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
